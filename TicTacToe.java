ba056c231b7361af8ffcf675559167acc7f088a2
SANJAYA KUMAR NAYAK <119873469+Sanjayaa02@users.noreply.github.com>
2023-11-26 15:19:03 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            printBoard();
+            int[] move = getPlayerMove(scanner);
+            makeMove(move[0], move[1]);
+
+            if (checkWin() || checkTie()) {
+                printBoard();
+                System.out.println("Game over!");
+                break;
+            }
+
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+        }
+
+        scanner.close();
+    }
+
+    private static void printBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println();
+            System.out.println("-------------");
+        }
+    }
+
+    private static int[] getPlayerMove(Scanner scanner) {
+        int row, col;
+        while (true) {
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+            row = scanner.nextInt();
+            col = scanner.nextInt();
+
+            if (isValidMove(row, col)) {
+                break;
+            } else {
+                System.out.println("Invalid move. Try again.");
+            }
+        }
+        return new int[]{row, col};
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        return row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ';
+    }
+
+    private static void makeMove(int row, int col) {
+        board[row][col] = currentPlayer;
+    }
+
+    private static boolean checkWin() {
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+                    (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+                System.out.println("Player " + currentPlayer + " wins!");
+                return true;
+            }
+        }
+
+        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+                (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+            System.out.println("Player " + currentPlayer + " wins!");
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean checkTie() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        System.out.println("It's a tie!");
+        return true;
+    }
+}
